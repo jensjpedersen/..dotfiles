6b68fb5dc84fb5a2a02431f7f419e4ed1f7c2ced
Jens Junker Pedersen <jens.junker.pedersen@gmail.com>
2022-06-27 22:58:34 +0200

    Working autocmp

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,5 +1,24 @@
 
 require('plugins')
 
+vim.o.number=true
+vim.o.relativenumber=true
+
+-- Indent
+vim.o.tabstop=4
+vim.o.softtabstop=4
+vim.o.shiftwidth=4
+vim.o.expandtab=true
+vim.o.smartindent=true
+
+vim.o.signcolumn='yes'
+
 require('lsp')
+
+require('cmp_config')
+
+
+
+
+
 
--- .config/nvim/lua/plugins.lua
+++ .config/nvim/lua/plugins.lua
@@ -7,5 +7,27 @@ local use = require('packer').use
 require('packer').startup(function()
   use 'wbthomason/packer.nvim' -- Package manager
   use 'neovim/nvim-lspconfig' -- Configurations for Nvim LSP
+  use 'hrsh7th/cmp-nvim-lsp'
+  -- cmp
+  use 'hrsh7th/cmp-buffer'
+  use 'hrsh7th/cmp-path'
+  use 'hrsh7th/cmp-cmdline'
+  use 'hrsh7th/nvim-cmp'
+
+  -- For vsnip users.
+  -- use 'hrsh7th/cmp-vsnip'
+  -- use 'hrsh7th/vim-vsnip'
+
+  -- For luasnip users.
+  -- use 'L3MON4D3/LuaSnip'
+  -- use 'saadparwaiz1/cmp_luasnip'
+
+  -- For ultisnips users.
+  use 'SirVer/ultisnips'
+  use 'quangnguyen30192/cmp-nvim-ultisnips'
+
+  -- For snippy users.
+  -- use 'dcampos/nvim-snippy'
+  -- use 'dcampos/cmp-snippy'
 end)
 
