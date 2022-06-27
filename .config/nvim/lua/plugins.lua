6ca0f4594c031c952d6a6c696d1164f013e5e774
Jens Junker Pedersen <jens.junker.pedersen@gmail.com>
2022-06-27 17:44:11 +0200

    nvim start config

---
+++ .config/nvim/lua/plugins.lua
@@ -0,0 +1,10 @@
+-- This file can be loaded by calling `lua require('plugins')` from your init.vim
+
+-- Only required if you have packer configured as `opt`
+vim.cmd [[packadd packer.nvim]]
+
+local use = require('packer').use
+require('packer').startup(function()
+  use 'wbthomason/packer.nvim' -- Package manager
+  use 'neovim/nvim-lspconfig' -- Configurations for Nvim LSP
+end)
