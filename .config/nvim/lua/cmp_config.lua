76e729cf183a77b806ddf1481988a92f7befb024
Jens Junker Pedersen <jens.junker.pedersen@gmail.com>
2022-06-28 13:43:43 +0200

    general configs

--- .config/nvim/lua/cmp_config.lua
+++ .config/nvim/lua/cmp_config.lua
@@ -65,8 +65,17 @@ cmp.setup.cmdline(':', {
 
 -- Setup lspconfig.
 local capabilities = require('cmp_nvim_lsp').update_capabilities(vim.lsp.protocol.make_client_capabilities())
+
 -- Replace <YOUR_LSP_SERVER> with each lsp server you've enabled.
 require('lspconfig')['sumneko_lua'].setup {
-    capabilities = capabilities
+    capabilities = capabilities,
+    settings = {
+        Lua = {
+            diagnostics = {
+                globals = { 'vim' }
+            }
+        }
+    }
 }
+
 
--- .config/nvim/lua/config_general.lua
+++ .config/nvim/lua/config_general.lua
@@ -1,14 +1,39 @@
 
-vim.o.number=true
-vim.o.relativenumber=true
+vim.g.mapleader = " "
+vim.g.maplocalleader = ","
+
+vim.o.number = true
+vim.o.relativenumber = true
 
 -- Indent
-vim.o.tabstop=4
-vim.o.softtabstop=4
-vim.o.shiftwidth=4
-vim.o.expandtab=true
-vim.o.smartindent=true
+vim.o.tabstop = 4
+vim.o.softtabstop = 4
+vim.o.shiftwidth = 4
+vim.o.expandtab = true
+vim.o.smartindent = true
 
-vim.o.wrap=false
-vim.o.signcolumn='yes'
+-- Search
+vim.o.ignorecase = true
+vim.o.smartcase = true
 
+-- Backup
+vim.o.undodir = '/home/jensjp/.config/nvim/undodir/'
+vim.o.undofile = true
+
+-- style 
+vim.o.signcolumn = 'yes'
+-- vim.cursorline = 'yes'
+vim.o.colorcolumn = '80'
+
+-- Etc
+vim.o.clipboard = 'unnamedplus'
+vim.o.autochdir = true
+vim.o.wrap = false
+vim.o.mouse = 'a'
+
+vim.o.timeoutlen = 1000
+vim.o.ttimeoutlen = 0
+
+-- Remember folds and last position
+vim.api.nvim_command('autocmd BufWinLeave *.* mkview')
+vim.api.nvim_command('autocmd BufWinEnter *.* silent loadview')
