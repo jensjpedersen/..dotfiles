f185c3a178528a589c8bbd33a75cc30deb33b028
Jens Junker Pedersen <jens.junker.pedersen@gmail.com>
2022-06-30 21:39:46 +0200

    working matlab config

--- .config/nvim/ftplugin/matlab.lua
+++ .config/nvim/ftplugin/matlab.lua
@@ -7,20 +7,20 @@ vim.keymap.set('n', '<Leader>vi', ':VimuxInspectRunner<CR>', opts)
 vim.keymap.set('n', '<Leader>vk', ':VimuxInterruptRunner<CR>', opts)
 vim.keymap.set('n', '<Leader>vz', ':VimuxZoomRunner<CR>', opts)
 vim.keymap.set('n', '<leader>vj', ":w<CR>:VimuxRunCommand('run ' . bufname('%'))<CR>, opts", opts)
+
 vim.keymap.set('n', '<leader>vc', ":w<CR>:VimuxRunCommand('clc')<CR>:VimuxRunCommand('clear all')<CR>", opts)
 vim.keymap.set('n', '<leader>vw', ":w<CR>:VimuxRunCommand('workspace')<CR>", opts)
 vim.keymap.set('n', '<leader>vd', [[:norm "xyiw<CR>]] .. ":VimuxRunCommand('doc <C-R>x')<CR>", opts)
 
--- tmux debugg
-vim.keymap.set('n', '<leader>ds', ':w<CR>:!tmux send -t left dbquit Enter<CR>', opts)
-vim.keymap.set('n', '<leader>dn', ':w<CR>:!tmux send -t left dbcont Enter<CR>', opts)
-vim.keymap.set('n', '<leader>dc', ':w<CR>:!tmux send -t left "dbclear all" Enter<CR>', opts)
-vim.keymap.set('n', '<leader>de', ':w<CR>:!tmux send -t left "dbstop if error" Enter<CR>', opts)
+-- vim.keymap.set('n', '<leader>ds', ':w<CR>:call jobstart("tmux send -t left dbquit Enter")<CR>', opts)
+vim.keymap.set('n', '<leader>ds', ":call jobstart('tmux send -t left dbquit Enter')<CR>", opts)
+vim.keymap.set('n', '<leader>dn', ":call jobstart('tmux send -t left dbcont Enter')<CR>", opts)
+vim.keymap.set('n', '<leader>dc', [[:call jobstart("tmux send -t left 'dbclear all' Enter")<CR>]], opts)
+vim.keymap.set('n', '<leader>de', [[:call jobstart("tmux send -t left 'dbstop if error' Enter")<CR>]], opts)
 
 -- go to first instance of word  
 vim.keymap.set('n', 'gd', ':norm *<CR> :vimgrep /<c-r>//g %<CR>', opts)
 vim.keymap.set('n', 'gD', ':norm *<CR> :vimgrep /<c-r>//g **/*.m<CR>', opts)
-
 
 vim.cmd('set filetype=matlab')
 
@@ -35,15 +35,6 @@ autocmd Filetype matlab
     endif
 ]])
 
--- vim.cmd([[
--- function! OpenCompletion()
---     if !pumvisible() && ((v:char >= 'a' && v:char <= 'z') || (v:char >= 'A' && v:char <= 'Z'))
---         call feedkeys("\<C-x>\<C-o>", "n")
---     endif
--- endfunction
--- autocmd InsertCharPre * call OpenCompletion()
--- ]])
-
 -- Omni completion
 require'cmp'.setup {
   sources = {
@@ -52,8 +43,3 @@ require'cmp'.setup {
 }
 
 
--- require'cmp'.setup {
---     sources = {
---         { name = "cmp_matlab" },
---     }
--- }
