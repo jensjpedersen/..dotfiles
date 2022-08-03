a04cf02a2f543f55957f93b099558ddd3adc7758
Jens Junker Pedersen <jens.junker.pedersen@gmail.com>
2022-08-03 16:54:59 +0200

    iptyon startup script

---
+++ .config/ptpython/ipython_startup.py
@@ -0,0 +1,9 @@
+import IPython
+import numpy as np
+import matplotlib.pyplot as plt
+
+ip = IPython.terminal.interactiveshell.TerminalInteractiveShell()
+# IPython.get_ipython().run_line_magic('reload_ext', 'autoreload')
+# IPython.get_ipython().run_line_magic('autoreload', 2)
+ip.run_line_magic('reload_ext', 'autoreload')
+ip.run_line_magic('autoreload', 2)
