013e87be277537ca55a669aa4fa4c3fbf1008be7
Jens Junker Pedersen <jens.junker.pedersen@gmail.com>
2022-08-03 15:03:18 +0200

    menu script

---
+++ Scripts/Menu/suspend_wakeup.sh
@@ -0,0 +1,50 @@
+#!/bin/bash
+
+alarm=${1?"Error: requiers command line input - hh:mm"}
+
+# Set crontab
+IFS=':' read -r -a array <<< "$alarm"
+
+echo "${array[1]} ${array[0]} * * * jensjp /bin/bash ${HOME}/Scripts/Cronjobs/wakeup_music.sh" | sudo tee /etc/cron.d/alarm
+
+today=$(date -d "today ${1}" '+%s')
+now=$(date '+%s')
+
+# Set machine wakeup
+
+diff=5 # Wake pc int minutes before alarm
+if (( ${array[1]} >= $diff )); then
+    min=$(( ${array[1]} - $diff ))
+    wake="${array[0]}:$min"
+else
+    min=$(( 60 + ${array[1]} - $diff ))
+    wake="${array[0]}:$min"
+fi
+# Fails down to here
+
+if (( $now > $today)); then
+    sudo rtcwake -m no -l -t $(date -d "tomorrow ${wake}" '+%s')
+elif (( $now < $today )); then
+    sudo rtcwake -m no -l -t $(date -d "today ${wake}" '+%s')
+fi
+
+# Ready for next day  
+xrandr --output LVDS-1 --primary --mode 1920x1080 --pos 0x0 --rotate normal --output VGA-1 --off --output DP-1 --off --output DP-2 --off --output DP-3 --off
+sleep 2
+[[ $(pgrep mpv)  ]] && pkill mpv
+[[ $(pgrep kodi) ]] && pkill kodi
+
+# Bedtime rutine
+sleep 2m 
+xset dpms force off
+sleep 3m
+python $HOME/Scripts/TapoScripts/tapo.py n &
+sleep 5m 
+playerctl -p spotify pause
+mpv "/mnt/ssd/Meditation/Relaxing Sleep Music • Deep Sleeping Music, Relaxing Music, Stress Relief, Meditation Music (Flying)-1ZYbU82GVz4.m4a"
+xrandr --output LVDS-1 --primary --mode 1920x1080 --pos 0x0 --rotate normal --output VGA-1 --off --output DP-1 --off --output DP-2 --off --output DP-3 --off
+redshift -P -O 3000 -b 1
+sleep 5
+systemctl suspend
+
+
