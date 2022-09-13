ee29d866b3d4abcf43cae4feed36e10aa470a9e7
Jens Junker Pedersen <jens.junker.pedersen@gmail.com>
2022-09-13 21:39:57 +0200

    working exec_always script on i3 reload

--- .config/i3/Startup/exec_always.sh
+++ .config/i3/Startup/exec_always.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 sleep 1 
 if [[ -d ${HOME}/Projects/Lemonmenu ]]; then
-    setsid bash /home/jensjp/Projects/Lemonmenu/lemonmenu.sh
+    bash /home/jensjp/Projects/Lemonmenu/lemonmenu.sh &
 fi
