68d109633866eeb049e8849478d7ed1a0f7eb67d
Jens Junker Pedersen <jens.junker.pedersen@gmail.com>
2022-09-24 21:21:58 +0200

    changed git signs

--- .config/nvim/lua/plugins/git.lua
+++ .config/nvim/lua/plugins/git.lua
@@ -22,7 +22,13 @@ vim.keymap.set('n', '<localleader>q', ':bdelete fugitive:*<C-a><CR>', opts)
 -- Git signs
 require('gitsigns').setup{
     attach_to_untracked = false,
-
+    signs = {
+        add          = {hl = 'GitSignsAdd'   , text = '+', numhl='GitSignsAddNr'   , linehl='GitSignsAddLn'},
+        change       = {hl = 'GitSignsChange', text = '│', numhl='GitSignsChangeNr', linehl='GitSignsChangeLn'},
+        delete       = {hl = 'GitSignsDelete', text = '_', numhl='GitSignsDeleteNr', linehl='GitSignsDeleteLn'},
+        topdelete    = {hl = 'GitSignsDelete', text = '‾', numhl='GitSignsDeleteNr', linehl='GitSignsDeleteLn'},
+        changedelete = {hl = 'GitSignsChange', text = '~', numhl='GitSignsChangeNr', linehl='GitSignsChangeLn'},
+    },
     on_attach = function(bufnr)
         local gs = package.loaded.gitsigns
 
