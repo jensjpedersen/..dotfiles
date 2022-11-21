fa32cc4094093713356a3a84a611a475f2a02034
Jens Junker Pedersen <jens.junker.pedersen@gmail.com>
2022-11-21 08:07:55 +0100

    fix

--- Scripts/WakeupAlarm/wakeup_music.sh
+++ Scripts/WakeupAlarm/wakeup_music.sh
@@ -1,12 +1,7 @@
 #!/bin/bash
-
-/usr/bin/python3 /home/jensjp/Scripts/TapoScripts/tapo.py o &
-/usr/bin/python3 /home/jensjp/Scripts/TapoScripts/tapo.py l &
-
 # Start playlist and increase volume every 2. second
 export PULSE_RUNTIME_PATH="/run/user/$(id -u)/pulse/"
 export XDG_RUNTIME_DIR="/run/user/$(id -u)"
-redshift -P -O 3000 -b 1
 min_vol=10
 max_vol=100
 pactl set-sink-volume @DEFAULT_SINK@ $min_vol%
