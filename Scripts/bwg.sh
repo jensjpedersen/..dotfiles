c4cf69f20ef59e2a56e6d5f589edcfa30aceebb4
Jens Junker Pedersen <jens.junker.pedersen@gmail.com>
2022-11-25 18:41:24 +0100

    handle multiple instances

--- Scripts/bwg.sh
+++ Scripts/bwg.sh
@@ -1,21 +1,35 @@
 #!/bin/bash
+#
+if [[ -z $BW_SESSION ]]; then 
+    echo "Session variable does not exist." 
+    exit 1
+fi
+
 items=$(bw list items)
 names=$(printf "%s" $(bw list items) | jq '.[].name')
 select=$(echo "$names" | fzf)
 element=$(printf "%s" "$items" | jq ".[] | select(.name == ${select})")
-username=$(echo "$element" | jq '.login.username')
-password=$(echo "$element" | jq '.login.password')
+username=( $(echo "$element" | jq '.login.username') )
+password=( $(echo "$element" | jq '.login.password') )
+
+index=0
+if (( ${#username[@]} > 1 )); then
+    echo Found multiple usernames. Choose one: 
+
+    for i in $(seq 0 $((${#username[@]}-1))); do #${username[@]}
+        echo $i ${username[$i]}
+    done
+
+    read -N 1 index
+
+fi
 
 clipctl disable
-echo -n ${username:1:-1}| xclip -select clipboard
-echo "Copied Username"
+echo -n ${username[$index]:1:-1}| xclip -select clipboard
+echo -e "\nCopied Username."
 read
 
-echo -n ${password:1:-1} | xclip -select clipboard
-echo "Copied Password"
+echo -n ${password[$index]:1:-1} | xclip -select clipboard
+echo "Copied Password."
 clipctl enable
-
-
-
-
 
