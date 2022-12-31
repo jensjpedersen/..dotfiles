c55b0fae1b1afa5a194a50f56e7b3e85bab55b2d
Jens Junker Pedersen <jens.junker.pedersen@gmail.com>
2022-12-31 12:46:16 +0100

    changed to own vimwiki fork

--- .config/nvim/lua/plugins/writing.lua
+++ .config/nvim/lua/plugins/writing.lua
@@ -1,5 +1,6 @@
 
 local opts = { noremap=true, silent=false }
+local opts2 = { noremap=true, silent=true }
 
 -- Latex --
 
@@ -30,10 +31,29 @@ let g:vimwiki_list = [{'path': '~/vimwiki/', 'syntax': 'markdown', 'ext': '.md'}
                     \ {'path': '~/vimwiki2/', 'syntax': 'markdown', 'ext': '.md'}]
 ]]
 
-vim.cmd[[
-let g:vimwiki_table_mappings=0
-let g:vimwiki_table_auto_fmt=0
-]]
+
+-- vim.cmd[[
+-- let g:vimwiki_key_mappings =
+-- \ {
+-- \   'all_maps': 1,
+-- \   'global': 1,
+-- \   'headers': 1,
+-- \   'text_objs': 1,
+-- \   'table_format': 0,
+-- \   'table_mappings': 0,
+-- \   'lists': 1, 
+-- \   'links': 1,
+-- \   'html': 0,
+-- \   'mouse': 0,
+-- \ }
+-- ]]
+
+
+-- vim.cmd[[
+-- let g:vimwiki_table_mappings=0
+-- let g:vimwiki_table_auto_fmt=0
+-- ]]
+
 
 -- Better colorscheme gruvbox (last part of init)
 vim.cmd([[
@@ -52,6 +72,8 @@ hi VimwikiPre ctermfg=3
 -- Vimwiki bindings
 vim.api.nvim_set_keymap('n' , 'gh', '<Plug>VimwikiAddHeaderLevel', opts)
 
+-- vim.cmd([[let g:vimwiki_filetypes = ['markdown', 'latex', 'tex'] ]])
+
 
 -- Markdown
 -- vim.keymap.set('n', '<localleader>mm',  ':w<CR>:! pandoc % -t latex -o %:r.pdf <CR>', opts)
@@ -59,6 +81,7 @@ vim.api.nvim_set_keymap('n' , 'gh', '<Plug>VimwikiAddHeaderLevel', opts)
 -- vim.keymap.set('n', '<localleader>mv',  ':w<CR>:! mupdf %:r.pdf & <CR>', opts)
 -- vim.keymap.set('n', '<localleader>md',  ':w<CR>:! rm %:r.pdf <CR>', opts)
 
+-- Markdown bindings
 vim.keymap.set('n', '<localleader>mm',  ':MarkdownPreviewToggle<CR>', opts)
 vim.keymap.set('n', '<localleader>ms',  ':w<CR>:! pandoc % -t beamer -o %:r.pdf <CR>', opts) -- Make slide show'
 vim.keymap.set('n', '<localleader>mv',  ':MarkdownPreview<CR>', opts)
@@ -72,3 +95,31 @@ function OpenMarkdownPreview (url)
 endfunction
 let g:mkdp_browserfunc = 'OpenMarkdownPreview'
 ]]
+
+
+-- vim.cmd("let g:wiki_root = '~/wiki/wiki1'")
+-- vim.cmd("let g:wiki_ft_wiki = [{'path': '~/wiki/wiki1'}, {'path': '~/wiki/wiki2'}]")
+
+-- Wiki settings
+-- vim.cmd[[
+--     function! WikiRoot()
+--       let l:local = finddir('wiki', ';./')
+--       return !empty(l:local) ? l:local : '~/wiki'
+--     endfunction
+
+--     let g:wiki_root = 'WikiRoot'
+-- ]]
+
+-- vim.cmd[[
+-- let g:wiki_filetypes = ['md', 'tex']
+-- let g:wiki_link_extension = '.md'
+-- let g:wiki_link_target_type = 'md'
+-- ]]
+
+-- -- Wiki mappings
+-- vim.keymap.set('n', '<Tab>', '<Plug>(wiki-link-next)', opts2)
+-- vim.keymap.set('n', '<S-Tab>', '<Plug>(wiki-link-prev)', opts2)
+
+
+
+
