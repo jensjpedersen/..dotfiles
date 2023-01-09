a107561ac4177a0639ba93c74ccf9f122d657475
Jens Junker Pedersen <jens.junker.pedersen@gmail.com>
2023-01-09 13:43:52 +0100

    [Python] Added snippet for loading ipython ns + autoload pandas

--- .config/ptpython/ipython_startup.py
+++ .config/ptpython/ipython_startup.py
@@ -1,6 +1,10 @@
 import IPython
 import numpy as np
 import matplotlib.pyplot as plt
+import pandas as pd
+
+pd.set_option('display.max_rows', 2000)
+pd.set_option('display.max_columns', 2000)
 
 ip = IPython.terminal.interactiveshell.TerminalInteractiveShell()
 # IPython.get_ipython().run_line_magic('reload_ext', 'autoreload')
