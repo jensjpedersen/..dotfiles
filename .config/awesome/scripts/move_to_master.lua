e86311bb5fcb12941dde9c8ea4f490df3f3de376
Jens Junker Pedersen <jens.junker.pedersen@gmail.com>
2023-04-22 21:12:33 +0200

    [Awesome] Fixed move to master. Ignore Polybar

--- .config/awesome/scripts/move_to_master.lua
+++ .config/awesome/scripts/move_to_master.lua
@@ -7,8 +7,14 @@ local client_info = awful.client.idx(client.focus)
 local current_sum = client_info['col'] + client_info['idx']
 
 local count = 0
-tag = awful.screen.focused().selected_tag
+local tag = awful.screen.focused().selected_tag
 for _, c in ipairs(tag:clients()) do
+
+    -- Ignore clients with name polybar
+    if c.name:find("polybar") then goto continue end
+
+    -- naughty.notify({text = tostring(c.name)})
+
     local info = awful.client.idx(c)
     local sum = info['col'] + info['idx']
 
@@ -16,10 +22,12 @@ for _, c in ipairs(tag:clients()) do
         count = count + 1
     end
 
+    ::continue::
+
 end
 
 local c = client.focus
-for i = 1, count do
+for _ = 1, count do
     awful.client.swap.byidx(-1, c)
 end
 
