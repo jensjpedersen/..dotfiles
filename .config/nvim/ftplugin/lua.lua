6bbaf1b12f3d59b32bbb446fd130a6ce1bf1101c
jens junker pdersen <jens.junker.pedersen@gmail.com>
2023-05-24 11:48:26 +0000

    empty branch

--- .config/nvim/ftplugin/lua.lua
+++
@@ -1,3 +0,0 @@
-
-local opts = { noremap=true, silent=true, buffer=0 }
-vim.keymap.set('n', '<leader>x', ':w<CR>:!lua %<CR>', opts)
--- .config/ptpython/ipython_startup.py
+++
@@ -1,9 +0,0 @@
-import IPython
-import numpy as np
-import matplotlib.pyplot as plt
-
-ip = IPython.terminal.interactiveshell.TerminalInteractiveShell()
-# IPython.get_ipython().run_line_magic('reload_ext', 'autoreload')
-# IPython.get_ipython().run_line_magic('autoreload', 2)
-ip.run_line_magic('reload_ext', 'autoreload')
-ip.run_line_magic('autoreload', 2)
