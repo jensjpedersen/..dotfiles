da731276db1e166203a2cc122909f146562306c2
Jens Junker Pedersen <jens.junker.pedersen@gmail.com>
2023-05-24 19:30:52 +0200

    Updated install script

--- install_config.sh
+++ install_config.sh
@@ -1,30 +1,25 @@
 #!/bin/sh
 
-sudo apt install git tmux ranger fzf curl gcc
+sudo apt install git tmux ranger fzf curl cmake
+
+# Install syntching 
+if which syncthing >/dev/null; then  
+    ## Add the release PGP keys:
+    sudo curl -o /usr/share/keyrings/syncthing-archive-keyring.gpg https://syncthing.net/release-key.gpg
+    # Add the "stable" channel to your APT sources:
+    echo "deb [signed-by=/usr/share/keyrings/syncthing-archive-keyring.gpg] https://apt.syncthing.net/ syncthing stable" | sudo tee /etc/apt/sources.list.d/syncthing.list
+    # Add the "candidate" channel to your APT sources:
+    echo "deb [signed-by=/usr/share/keyrings/syncthing-archive-keyring.gpg] https://apt.syncthing.net/ syncthing candidate" | sudo tee /etc/apt/sources.list.d/syncthing.list
+    # Update and install syncthing:
+    sudo apt-get update
+    sudo apt-get install syncthing
+fi 
 
 # Install latest nvim
-curl -LO https://github.com/neovim/neovim/releases/latest/download/nvim.appimage
-chmod u+x nvim.appimage
+if [ ! -d "$HOME/neovim" ]; then
+    git clone https://github.com/neovim/neovim
+    cd neovim && make CMAKE_BUILD_TYPE=RelWithDebInfo 
+    sudo make install
+    cd "$HOME" 
+fi
 
-./nvim.appimage --appimage-extract
-./squashfs-root/AppRun --version
-
-# Optional: exposing nvim globally.
-sudo mv squashfs-root /
-sudo ln -s /squashfs-root/AppRun /usr/bin/nvim
-
-# syntching 
-
-## Add the release PGP keys:
-sudo curl -o /usr/share/keyrings/syncthing-archive-keyring.gpg https://syncthing.net/release-key.gpg
-
-# Add the "stable" channel to your APT sources:
-echo "deb [signed-by=/usr/share/keyrings/syncthing-archive-keyring.gpg] https://apt.syncthing.net/ syncthing stable" | sudo tee /etc/apt/sources.list.d/syncthing.list
-
-
-# Add the "candidate" channel to your APT sources:
-echo "deb [signed-by=/usr/share/keyrings/syncthing-archive-keyring.gpg] https://apt.syncthing.net/ syncthing candidate" | sudo tee /etc/apt/sources.list.d/syncthing.list
-
-# Update and install syncthing:
-sudo apt-get update
-sudo apt-get install syncthing
