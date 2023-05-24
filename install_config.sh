f90b954b377c58ec4d231353fc71a91bcb2b7372
Jens Junker Pedersen <jens.junker.pedersen@gmail.com>
2023-05-24 16:48:16 +0200

    Fixed instal script

--- install_config.sh
+++ install_config.sh
@@ -6,6 +6,13 @@ sudo apt install git tmux ranger fzf curl gcc
 curl -LO https://github.com/neovim/neovim/releases/latest/download/nvim.appimage
 chmod u+x nvim.appimage
 
+./nvim.appimage --appimage-extract
+./squashfs-root/AppRun --version
+
+# Optional: exposing nvim globally.
+sudo mv squashfs-root /
+sudo ln -s /squashfs-root/AppRun /usr/bin/nvim
+
 # syntching 
 
 ## Add the release PGP keys:
