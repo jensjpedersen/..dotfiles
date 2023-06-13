bcd7b3e583ee0063756e8b96506b834e53212661
Jens Junker Pedersen <jens.junker.pedersen@gmail.com>
2023-06-13 22:26:05 +0200

    [Scripts] Added more consoles in gaming menu

--- Scripts/GameMenu/game_menu.sh
+++ Scripts/GameMenu/game_menu.sh
@@ -12,59 +12,51 @@ fi
 
 console_dir=( $(ls $games_dir) )
 root_dir=$(dirname $(realpath $0))
-thumb_dir="$root_dir/libretro-thumbnails"
 
 
 function download_thumbnails { 
-    if [ ! -d "$thumb_dir" ]; then
-        cd $root_dir
-        git clone https://github.com/libretro-thumbnails/libretro-thumbnails
-    fi
 
-    if [ ! -d "thumbnails" ]; then
-        cd $root_dir
+    if [ ! -d "$root_dir/thumbnails" ]; then
         mkdir thumbnails
     fi
 
 
     for d in "${console_dir[@]}"; do 
-        console=$(echo $d | tr '_' ' ')
-        console_url=$(ls $thumb_dir | grep -i "$console" | sort | head -n 1)
-        console_url=$(echo $console_url | tr ' ' '_')
-
 
         for f in "$games_dir""$d"/*; do 
 
-
-            # if empty console url 
-            [[ -z "$console_url" ]] && break
-
             file_name="${f##*/}"
 
-            # echo $console_url
-
-            if [ $console_url = "Nintendo_-_GameCube" ]; then
-
-                if [ ! -d "thumbnails/$console" ]; then
-                    mkdir "thumbnails/$console"
-                fi
-
-                # remove .iso .nkit
-                game_name=$(echo "$file_name" | sed -e 's/\.iso//' -e 's/\.nkit//')
-                # game_url=$(echo "$game_name" | sed -e 's/ /\%20/g')
-                img_url="$game_name"".png"
-
-                url="https://raw.githubusercontent.com/libretro-thumbnails/""$console_url""/master/Named_Boxarts/""$img_url"
-
-
-                if [ ! -f "thumbnails/$console/$img_url" ]; then 
-                    wget -q --directory-prefix "thumbnails/$console" "$url" || echo "download failed: $game_name"
-                fi
-
-
-                # ping -c 1 "$url" && echo $game_name
-                # wget -q "$url" || echo "download failed: $game_name"
+            if [ ! -d "thumbnails/$d" ]; then
+                mkdir "thumbnails/$d"
             fi
+
+            if [ $d = "gamecube" ]; then
+
+                game_name=$(echo "$file_name" | sed -e 's/\.iso//' -e 's/\.nkit//')
+                img_url="$game_name"".png"
+                url="https://raw.githubusercontent.com/libretro-thumbnails/Nintendo_-_GameCube/master/Named_Boxarts/""$img_url"
+
+            elif [ $d = 'play_station_2' ]; then 
+
+                game_name=$(echo "$file_name" | sed -e 's/\.iso//')
+                img_url="$game_name"".png"
+                url="https://raw.githubusercontent.com/libretro-thumbnails/Sony_-_PlayStation_2/733507283dd0f52e3b8a47fe0a139be13f82903a/Named_Boxarts/$img_url"
+
+            elif [ $d = 'nintendo_64' ]; then 
+
+                game_name=$(echo "$file_name" | sed -e 's/\.z64//')
+                img_url="$game_name"".png"
+                url="https://raw.githubusercontent.com/libretro-thumbnails/Nintendo_-_Nintendo_64/c9278f93bfbcb41e4e728b952b25e6cc2ab6830f/Named_Boxarts/$img_url"
+
+            else
+                break 
+            fi
+
+            if [ ! -f "thumbnails/$d/$img_url" ]; then 
+                wget -q --directory-prefix "thumbnails/$d" "$url" || echo "download failed: $game_name"
+            fi
+
 
         done
 
@@ -83,12 +75,13 @@ function game_picker {
     [[ -z "$choice" ]] && exit 0
 
 
+    file_search=$(echo "$choice" | sed -e 's/\.png//')
+    file_search=$(basename "$file_search")
+
+    result=$(find $games_dir -name "*$file_search*")
+
     if echo $choice | grep -q "gamecube"; then
 
-        file_search=$(echo "$choice" | sed -e 's/\.png//')
-        file_search=$(basename "$file_search")
-
-        result=$(find $games_dir -name "*$file_search*")
         dolphin-emu -e "$result" &
 
         sleep 2 
@@ -96,6 +89,19 @@ function game_picker {
         id=$(xdotool search --name "dolphin-emu" | head -n 1)
 
         xdotool key --window $id 'super+f'
+
+
+
+    elif echo $choice | grep -q "nintendo_64"; then 
+        # Nintendo 64
+
+        if which org.libretro.RetroArch && [ -f "${HOME}/.var/app/org.libretro.RetroArch/config/retroarch/cores/mupen64plus_next_libretro.so" ]; then
+            org.libretro.RetroArch -f -L "${HOME}/.var/app/org.libretro.RetroArch/config/retroarch/cores/mupen64plus_next_libretro.so" "$result" &
+        fi
+
+    elif echo $choice | grep -q "play_station_2"; then
+        # Playstation 2
+        which pcsx2-qt && pcsx2-qt "$result" &
 
 
 
