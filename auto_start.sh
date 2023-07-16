629dc9673d5b2a3b5399e734e2a69f0c736d0108
Jens Junker Pedersen <jens.junker.pedersen@gmail.com>
2023-07-16 12:36:09 +0200

    [Startup] Thinkcentre auto start script.

---
+++ auto_start.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+sleep 5 
+
+pgrep syncthing || syncthing -no-browser &
+
+# Set keyboard layout
+setxkbmap -layout no
+sleep 1
+# setxkbmap -option caps:escape
+sleep 1
+setxkbmap -option lv3:lalt_switch
+#sleep 1
+#setxkbmap -option ctrl:rctrl_ralt
+sleep 1
+setxkbmap -model pc105
+sleep 1
+setxkbmap -option caps:escape
+sleep 1
+xset r rate 500 50
+sleep 1 
+# xkbcomp $HOME/.config/i3/Startup/.Xkeymap $DISPLAY
+xmodmap "$HOME/.config/i3/Startup/.Xmodmap"
