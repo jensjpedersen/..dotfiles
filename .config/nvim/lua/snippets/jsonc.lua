f26522b94812cdb673f3e0c5b315a62afbc06f72
Jens Junker Pedersen <jens.junker.pedersen@gmail.com>
2024-01-03 10:27:38 +0100

    feat(neovim): json config snippets

---
+++ .config/nvim/lua/snippets/jsonc.lua
@@ -0,0 +1,22 @@
+
+return {
+
+s({ trig="sc", name="styled component config" }, fmt([[
+    {
+      "compilerOptions": {
+        "plugins": [
+          {
+            "name": "@styled/typescript-styled-plugin"
+          }
+        ]
+      }
+    }
+    <>
+    ]],
+    { i(0) },
+    { delimiters='<>' }
+)),
+
+
+
+}
