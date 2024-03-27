f5a864405424b5192a0c8e182492a4fb325591d9
Jens Junker Pedersen <jens.junker.pedersen@gmail.com>
2024-03-27 20:04:25 +0100

    feat(py conf): autoreload ipython modules

--- .config/ptpython/ipython_startup.py
+++ .config/ptpython/ipython_startup.py
@@ -9,8 +9,3 @@ pretty.install()
 pd.set_option('display.max_rows', 2000)
 pd.set_option('display.max_columns', 2000)
 
-# ip = IPython.terminal.interactiveshell.TerminalInteractiveShell()
-# IPython.get_ipython().run_line_magic('reload_ext', 'autoreload')
-# IPython.get_ipython().run_line_magic('autoreload', 2)
-# ip.run_line_magic('reload_ext', 'autoreload')
-# ip.run_line_magic('autoreload', 2)
--- .ipython/profile_default/ipython_config.py
+++ .ipython/profile_default/ipython_config.py
@@ -1054,6 +1054,12 @@ c.TerminalInteractiveShell.editing_mode = 'vi'
 # c.StoreMagics.autorestore = False
 
 
+# autoreload modules in ipython
+c.InteractiveShellApp.exec_lines = []
+c.InteractiveShellApp.exec_lines.append('%load_ext autoreload')
+c.InteractiveShellApp.exec_lines.append('%autoreload 2')
+c.InteractiveShellApp.exec_lines.append('print("Warning: disable autoreload in ipython_config.py to improve performance.")')
+
 
 # Custom prompt
 try:
